58b9e69d8e23d57906159e6fac92a16f57f48dad
yusril maqoshidana <yusrilkingsr07@gmail.com>
2025-03-05 09:27:48 +0700

    [fix] check point 1

--- app/src/main/java/id/usereal/mysportapp/MainActivity.kt
+++ app/src/main/java/id/usereal/mysportapp/MainActivity.kt
@@ -1,8 +1,17 @@
 package id.usereal.mysportapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
@@ -15,60 +24,15 @@ import id.usereal.mysportapp.view.home.HomeFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var splitInstallManager: SplitInstallManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        splitInstallManager = SplitInstallManagerFactory.create(this)
-
-        if (savedInstanceState == null) {
-            loadFragment(HomeFragment())
-        }
-
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> loadFragment(HomeFragment())
-                R.id.nav_favorite -> loadFavoriteFeature()  // Load Modul Favorite Secara Dinamis
-            }
-            true
-        }
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
-    }
-
-    private fun loadFavoriteFeature() {
-        val moduleName = "favorite"
-        if (splitInstallManager.installedModules.contains(moduleName)) {
-            try {
-                val fragmentClass =
-                    Class.forName("id.usereal.mysportapp.favorite.ui.FavoriteFragment")
-                val fragment = fragmentClass.getDeclaredConstructor().newInstance() as Fragment
-                loadFragment(fragment)
-            } catch (e: Exception) {
-                Toast.makeText(this, "Gagal memuat modul Favorite", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            val request = SplitInstallRequest.newBuilder()
-                .addModule(moduleName)
-                .build()
-
-            splitInstallManager.startInstall(request)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "Module Favorite Terinstall", Toast.LENGTH_SHORT).show()
-                    loadFavoriteFeature()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Gagal Menginstall Modul Favorite", Toast.LENGTH_SHORT)
-                        .show()
-                }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
